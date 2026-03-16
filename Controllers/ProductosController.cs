8e0918697ba700390ea09e51571dee6b1b257f9f
EFRAIN_A_MANOTAS_C <efraina.manotasc@gmail.com>
2026-03-15 22:26:37 -0500

    Otro update

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,54 @@
+﻿using ECOMMERCE_NEXOSOFT.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECOMMERCE_NEXOSOFT.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly NexosoftDbContext _context;
+
+        public ProductosController(NexosoftDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? categoria)
+        {
+            var query = _context.Productos
+                .Include(p => p.IdCategoriaNavigation)
+                .AsQueryable();
+
+            if (categoria.HasValue)
+            {
+                query = query.Where(p => p.IdCategoria == categoria.Value);
+            }
+
+            var productos = await query.ToListAsync();
+
+            ViewBag.CategoriaSeleccionada = categoria;
+
+            return View(productos);
+        }
+
+        public async Task<IActionResult> Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.IdCategoriaNavigation)
+                .Include(p => p.Stock)
+                .FirstOrDefaultAsync(p => p.IdProducto == id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ECOMMERCE_NEXOSOFT.Models;
+
+namespace ECOMMERCE_NEXOSOFT.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Categorium> Categorias { get; set; } = new();
+
+        public List<Producto> ProductosDestacados { get; set; } = new();
+    }
+}
