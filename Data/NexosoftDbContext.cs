58e5c12130e341217423349dd94a06ad25284738
Milord-Taro <efraina.manotasc@gmail.com>
2026-04-18 11:39:17 -0500

    Change MySQL connection string to use placeholders
    Updated the connection string to use placeholder values for user and password.

--- Data/NexosoftDbContext.cs
+++ Data/NexosoftDbContext.cs
@@ -59,7 +59,7 @@ public partial class NexosoftDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=NexosoftDB;user=root;password=123", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;port=3306;database=NexosoftDB;user=YOUR_USER;password=YOUR_PASSWORD;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
